f448ec7950edca91dea1ed4683d08b0443ed180d
NHTung-0801 <kiritohackiem05@gmail.com>
2025-11-19 00:19:05 +0700

    update src/shared

---
+++ src/shared/Constants.java
@@ -0,0 +1,8 @@
+package shared;
+
+public class Constants {
+    public static final String HOST = "localhost"; // Hoặc IP của Server
+    public static final int PORT = 9999;
+    public static final int BOARD_SIZE = 20; // Kích thước bàn cờ
+    public static final int UDP_PORT = 7777; // có thể đổi
+}
---
+++ src/shared/GameMessage.java
@@ -0,0 +1,42 @@
+package shared;
+
+import java.io.Serializable;
+
+/**
+ * Lớp này định nghĩa đối tượng tin nhắn
+ * mà Client và Server sẽ trao đổi.
+ * Phải implement Serializable.
+ */
+public class GameMessage implements Serializable {
+    
+    // Định nghĩa các loại tin nhắn
+    public static final String MSG_PLAY_AGAIN_REQUEST = "PLAY_AGAIN";
+    public static final String MSG_PLAY_AGAIN_DECLINE = "PLAY_AGAIN_DECLINE"; 
+    public static final String MSG_FIND_NEW_OPPONENT  = "FIND_NEW_OPPONENT";
+    public static final String MSG_PING = "PING";
+    public static final String MSG_PONG = "PONG";
+    public static final String MSG_GAME_RESET = "GAME_RESET";
+    public static final String MSG_PARTNER_QUIT = "PARTNER_QUIT";
+    public static final String MSG_CONNECT = "CONNECT";
+    public static final String MSG_MOVE = "MOVE";
+    public static final String MSG_UPDATE_BOARD = "UPDATE_BOARD";
+    public static final String MSG_WIN = "WIN";
+    public static final String MSG_LOSE = "LOSE";
+    public static final String MSG_CHAT = "CHAT";
+
+    private String type; // Loại tin nhắn (dùng các hằng số ở trên)
+    private Object data; // Dữ liệu đi kèm (ví dụ: tọa độ, nội dung chat)
+
+    public GameMessage(String type, Object data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Object getData() {
+        return data;
+    }
+}
